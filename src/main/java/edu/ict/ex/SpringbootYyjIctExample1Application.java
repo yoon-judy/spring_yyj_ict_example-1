8a23f1d2172060ded3224376517f549a1d3156b5
WIN-6TB7VE6HUH0\ICT02_10 <svmesvu@gmail.com>
2024-09-11 11:42:25 +0900

    gggggggggggggg

---
+++ src/main/java/edu/ict/ex/SpringbootYyjIctExample1Application.java
@@ -0,0 +1,13 @@
+package edu.ict.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootYyjIctExample1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootYyjIctExample1Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ict/ex/SpringbootYyjIctExample1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ict.ex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootYyjIctExample1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
